1ce898cf592abf935df3eabec3a4a54137f3d659
janevsky <baze.janevski@gmail.com>
2020-12-20 03:01:12 +0100

    Zadaca1

---
+++ Zadaca1/app.js
@@ -0,0 +1,33 @@
+//Da se registrirate na hackerrank, resete 3 zadaci (2h)
+
+/*
+
+1. Simple Array Sum
+
+function simpleArraySum(ar) {
+    let sum = 0;
+    for (let i=0; i < ar.length; i++)
+    
+    return ar.reduce((acc, current) => acc += current, 0 )
+
+};
+
+
+2. A Very Big Sum
+
+function aVeryBigSum(ar) {
+    let sum = 0;
+    for (let i = 0; i < ar.length; i++){
+        sum += ar[i];
+    }
+return sum;
+}
+
+3. Solve Me First
+
+function solveMeFirst(a, b) {
+  return a + b;
+}
+
+
+*/
