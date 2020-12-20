1e770669d6b497c0a020895777fee0712408efaf
janevsky <baze.janevski@gmail.com>
2020-12-20 02:50:19 +0100

    Create app.js

---
+++ Zadaca2/app.js
@@ -0,0 +1,50 @@
+//Da se iskoristat 5 array funkcii
+
+
+// 1st
+let buttons = ['space', 'ctrl', 'shift', 'alt'];
+
+function firstArray() {
+
+    console.log(Array.isArray(buttons));
+
+};
+
+firstArray();
+
+// 2nd
+
+function secondArray() {
+
+    console.log(buttons.filter(button => button.length > 4));
+
+};
+secondArray();
+
+// 3rd 
+
+function thirdArray() {
+
+    buttons.forEach(element => console.log(element));
+
+};
+thirdArray();
+
+// 4th
+
+function fourthArray() {
+
+    buttons.sort();
+    console.log(buttons);
+}
+fourthArray();
+
+// 5th
+
+function fifthArray() {
+
+    buttons.push('tab');
+    console.log(buttons);
+
+};
+fifthArray();
