be46c8e44196b68fe64ea1d025dcac4605035886
janevsky <baze.janevski@gmail.com>
2021-01-25 16:41:28 +0100

    update

--- cas3/routes/index.js
+++ cas3/routes/index.js
@@ -1,41 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-// simulacija na baze
-const players = [{ name: 'Auba', goals_scoored: 5 }, { name: 'Laca', goals_scoored: 10 }]
+// simulacija na baza
+const players = []
 
-router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Players App',
-        players: players
-    });
-});
-
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create player' })
-});
-
-router.post('/players', (req, res) => {
-    req.body = {
-        name: 'test',
-        goals_scoored: 0
-    }
-    router.get('/players/:id', (req, res) => {
-        // da napiseme kod koj od baza kje go zeme playerot so ova ID: req.params.id
-        const player = players.find(player => {
-            return player.id == req.params.id
-        })
+router
+      .get('/', (req, res) => {
+        res.render('index', { title: 'Players App', players });
+      })
+      .get('/create', (req, res) => {
+        res.render('create', { title: 'Create player' })
+      })
+      .get('/players/:id', (req, res) => {
+        const player = players.find(player => player.id == req.params.id)
 
         res.render('edit', { title: 'Update player', player })
-    })
-    const player = {...req.body, id: players.length + 1 }
-    players.push(player)
-    res.redirect('/');
-    /* isto no po prosto kako gore navedenoto, destrukcija
-    const player = req.body;
-    player.id = players.length + 1;*/
-})
-router.get('/players/:id')
-    // ROUTE PARAMETERS
+      })
+      .post('/players', (req, res) => {
+        const player = { ...req.body, id: players.length + 1 }
+        players.push(player)
+        res.redirect('/')
+      })
 
 module.exports = router;
